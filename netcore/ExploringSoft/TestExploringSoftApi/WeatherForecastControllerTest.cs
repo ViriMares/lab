e360ea02571515982fe4e3ce8ef16734112aca85
ViriMares <virimaresoui@gmail.com>
2025-04-08 15:17:22 -0600

    added unit test for controller

---
+++ netcore/ExploringSoft/TestExploringSoftApi/WeatherForecastControllerTest.cs
@@ -0,0 +1,23 @@
+using ExploringSoft;
+using ExploringSoft.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace TestExploringSoftApi
+{
+    public class WeatherForecastControllerTest
+    {
+        [Fact]
+        public void Test_WeatherForecastController()
+        {
+            // Arrange
+            var logger = new Mock<ILogger<WeatherForecastController>>();
+            var controller = new WeatherForecastController(logger.Object);
+            // Act
+            var result = controller.Get();
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<WeatherForecast[]>(result);
+        }
+    }
+}
